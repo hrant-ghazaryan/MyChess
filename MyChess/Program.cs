2094816a78709f3a3df76f434988acfe76f51bf4
hrant-ghazaryan <hrant2004hrant@gmail.com>
2026-02-23 19:47:56 +0400

    Instantiate more chess pieces in Program.cs
    Added additional chess pieces: Rook, Bishop, King, Knight, and Pawn.

--- MyChess/Program.cs
+++ MyChess/Program.cs
@@ -1,5 +1,10 @@
 ﻿using ChessLibrary;
 
 Queen queen = new Queen();
+Rook rook = new Rook();
+Bishop bishop = new Bishop();
+King king = new Bishop();
+Knight knight = new Knight();
+Pawn pawn = new Pawn();
 bool b = queen.IsMovePossible("A1", "A8");
 Console.WriteLine(b);
