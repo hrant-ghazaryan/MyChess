3d18caaaf3c00181a16fc2633a268e557a5a71e7
hrant-ghazaryan <hrant2004hrant@gmail.com>
2026-02-23 03:28:29 -0800

    l

--- ChessLibrary/Figure/Pawn.cs
+++ ChessLibrary/Figure/Pawn.cs
@@ -9,8 +9,10 @@ namespace ChessLibrary
         public bool moveBlock;
         public Location location;
         public Color color;
-        public bool IsMovePossible(Location start, Location target)
+        public bool IsMovePossible(string value1, string value2)
         {
+            Location start = new Location(value1);
+            Location target = new Location(value2);
             if (color == Color.White)
             {
                 if (target.X - start.X == 0 && target.Y - start.Y == 1)
